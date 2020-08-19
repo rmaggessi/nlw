d901258ddd387f9348c7241d597df4cb2859e387
rmaggessi <rodrigomaggessi@gmail.com>
2020-08-19 01:54:40 -0300

    Added for loop/fixed whatsapp button

--- src/server.js
+++ src/server.js
@@ -20,7 +20,19 @@ const proffys = [
         weekday: [1],
         time_from: [720],
         time_to: [1220]
+    },
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "1199502341",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
     }
+    
 ]
 
 function pageLanding(req, res){
