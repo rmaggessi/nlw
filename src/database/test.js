205f5d56559b310dd83b98f8cc54f5dfb1bf6ed9
rmaggessi <rodrigomaggessi@gmail.com>
2020-08-29 12:36:42 -0300

    Data saving test

--- src/database/test.js
+++ src/database/test.js
@@ -1,1 +1,34 @@
-const db = require('./db')
+const Database = require('./db')
+
+Database.then((db) =>{
+    //Inserir dados
+
+    proffy = {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "1199502341",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    }
+
+    classValue = {
+        subject: "Química",
+        cost: "20",
+        //o proffy id virá pelo banco de dados
+    }
+
+    classSchedule = [
+        //class_id virá pelo banco de dados, após cadastrarmos a aula
+        {
+            weedkay: 1,
+            time_from: 720,
+            time_to: 1220
+        },
+        {
+            weedkay: 0,
+            time_from: 520,
+            time_to: 1220
+        }
+    ]
+    
+    //Consultar os dados inseridos
+})
